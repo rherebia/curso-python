2a503927be01690b3ff2c9d0a772590b8e4ed4df
Renato Herebia <rherebia@gmail.com>
2017-01-04 13:01:59 -0300

    Assistindo aula 6

---
+++ lesson-6_example-1.py
@@ -0,0 +1,11 @@
+print ("Test IF / ELSE")
+
+velocity1 = input("Enter your velocity: ")
+velocity1 = int(velocity1)
+
+if velocity1 > 100 :
+    print ("Caution! You are too fast!!!")
+else :
+    print ("Ok! You are with good velocity.")
+    
+print ("Finish")
