f3b5e9e04ac0b4e283c1ba84c1e5c05b1bfa25c9
Renato Herebia <rherebia@gmail.com>
2017-01-04 13:35:01 -0300

    Assistindo aula 9

---
+++ lesson-9_example-1.py
@@ -0,0 +1,11 @@
+print("Teste Logical Operators")
+
+if 1 == 1 and 2 == 2 :
+    print("Condition is true")
+else :
+    print("Condition is false")
+    
+if 1 == 1 and 2 == 3 :
+    print("New condition is true")
+else :
+    print("New condition is false")
