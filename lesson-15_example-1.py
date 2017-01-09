2116fc19681e5d78d18177bcd724091d006d2d85
Renato Herebia <rherebia@gmail.com>
2017-01-09 10:20:06 -0300

    Assistindo aula 15

---
+++ lesson-15_example-1.py
@@ -0,0 +1,18 @@
+print("Test Tuples")
+
+myTuple = 'Euclides', 2015, 'Devmedia', 3.14, 2.71, 'X'
+
+print("\nItems in myTuple: ", myTuple)
+print("\nQuantity of items in myTuple is ", len(myTuple))
+print("\nElement in position 3: ", myTuple[3])
+
+myTuple2 = (1, 'Fiat', 'Uno', 1.0, 2010)
+
+print("\nItems in myTuple2: ", myTuple2)
+print("\nQuantity of items in myTuple2 is ", len(myTuple2))
+
+# Tuple de um único elemento para diferenciar de variável comum
+myTuple3 = 'XYZ',
+
+print("\nItems in myTuple3: ", myTuple3)
+print("\nQuantity of items in myTuple3 is ", len(myTuple3))
---
+++ lesson-15_example-3.py
@@ -0,0 +1,12 @@
+print("Test tuples")
+
+myCarTuple = ('Ford', 'Focus', 2013, 2.0, 'Black')
+
+# desempacotamento
+(automaker, model, year, cc, color) = myCarTuple
+
+print("Automaker: ", automaker)
+print("Model: ", model)
+print("Year: ", year)
+print("cc: ", cc)
+print("color: ", color)
